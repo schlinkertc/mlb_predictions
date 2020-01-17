a6906dc0ca984837c6393d2a90ff6d02797ae238
Charlie Schlinkert <schlinkertc@gmail.com>
2020-01-17 11:09:26 -0500

    organizing..

--- functions.py
+++ functions.py
@@ -19,3 +19,34 @@ def read_gamePks():
             seasonPks = list(reader)
         gamePks[season[0]] = [item for sublist in seasonPks for item in sublist]
     return gamePks
+
+def getGame_df():    
+    game_df = pd.read_csv('games_file.csv')
+
+    game_df['wind'].fillna('unknown mph',inplace=True)
+
+    game_df['wind_speed']=game_df.apply(lambda x: x['wind'].split('mph')[0],axis=1)
+
+    game_df['wind_speed'].replace('unknown ',np.nan,inplace=True)
+
+    game_df['wind_speed']=game_df.apply(lambda x: int(x['wind_speed']) if type(x['wind_speed'])==str else x['wind_speed'],axis=1)
+
+    game_df['wind_direction']=game_df.apply(lambda x: x['wind'].split('mph')[1],axis=1)
+
+    def parse_windDir(string):
+        try:
+            string = string.split(', ')[1]
+        except:
+            pass
+        if string == '':
+            string='Calm'
+        if string=='none':
+            string='Calm'
+        if string=='None':
+            string='Calm'
+        return string 
+
+    game_df['wind_direction']=game_df.apply(lambda x: parse_windDir(x['wind_direction']),axis=1)
+    game_df.drop(columns=['wind'],inplace=True)
+
+    return game_df
